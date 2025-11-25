a5dd06e7d125c8a0db38d01466d604a72eb587ee
Mihvv <mihvd@proton.me>
2025-11-25 11:02:38 +0100

    C 4_1.2

--- index.js
+++ index.js
@@ -27,7 +27,30 @@ app.get('/math/circle/:r', (req, res) => {
   res.json(result);
 });
 
-//TODO2
+// zadanie 4_1.2
+app.get('/math/rectangle/:width/:height', (req, res) => {
+  const width = Number(req.params.width);
+  const height = Number(req.params.height);
+
+  if (isNaN(width) || width <= 0 || isNaN(height) || height <= 0) {
+    return res.status(400).json({
+      error: "Invalid input"
+    });
+  }
+
+  const area = width * height;
+  const perimeter = 2 * (width + height);
+
+  const result = 
+  {
+    width: width,
+    height: height,
+    area: area,
+    perimeter: perimeter
+  };
+
+  res.json(result);
+});
 
 
 //TODO3
