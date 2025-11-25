6113580b1333e587baf503a892a4e9fb5874706c
Mihvv <mihvd@proton.me>
2025-11-25 10:58:22 +0100

    C 4_1.1

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const app = express();
 
 // zadanie 4_1.1
 app.get('/math/circle/:r', (req, res) => {  
+  const r = Number(req.params.r);
+
+  if (isNaN(r) || r <= 0)
+  {
+    return res.status(400).json({
+      error: "Invalid input"
+    });
+  }
+
+  const area = Math.PI * r * r;
+  const circumference = 2 * Math.PI * r;
+
+  const result =
+  {
+    radius: r,
+    area: area.toFixed(2),
+    circumference : circumference.toFixed(2)
+  };
+  
   res.json(result);
 });
 
